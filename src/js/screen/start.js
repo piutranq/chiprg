758fe7db29f02df279d70d00a708e9950da07745
Piu Tranquillo <piutranq@hotmail.com>
2017-04-06 19:31:22 +0900

    비트맵 폰트 기능과 폰트 2종 추가

--- src/js/screen/start.js
+++ src/js/screen/start.js
@@ -1,11 +1,20 @@
 var screenStart = {
+
   name: "screenStart",
   buttonGoMenu: "",
+
+  ascii: ' !"#$%&\'()*+,-./\n0123456789:;<=>?\n@ABCDEFGHIJKLMNO\nPQRSTUVWXYZ[\\]^_\n`abcdefghijklmno\npqrstuvwxyz{|}~ ',
+
   preload: function(){
+
+    game.load.bitmapFont('font1014', 'assets/fonts/font1014.png', 'assets/fonts/font1014.xml');
+    game.load.bitmapFont('font1418', 'assets/fonts/font1418.png', 'assets/fonts/font1418.xml');
     game.load.image('redbutton','assets/img/redbutton.png');
+
   },
   create: function(){
-    var title = game.add.text(0, 0, this.name, { font: "14px Arial", fill: "#ffffff"});
+    var ascii1014 = game.add.bitmapText(0, 0,'font1014', this.ascii,  14)
+    var ascii1014 = game.add.bitmapText(0, 86,'font1418', this.ascii,  18)
     var buttonStart = game.add.button(
       game.world.centerX-32, game.world.centerY-18,
       'redbutton', this.goMenu, this);
